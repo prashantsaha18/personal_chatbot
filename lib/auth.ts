cb81cfce162ab8ba24f5abe2e0428ff57c5e58eb
prashantsaha18 <psaha00018@gmail.com>
2026-05-06 21:53:04 +0530

    Added new features

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,4 +1,4 @@
-import { NextAuthOptions } from "next-auth";
+/* import { NextAuthOptions } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 
 export const authOptions: NextAuthOptions = {
@@ -20,3 +20,4 @@ export const authOptions: NextAuthOptions = {
     },
   },
 };
+/* this is the auth file */
